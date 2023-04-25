f7213355a65ab6a910acdfae36ae215fd8b77dae
gafur07 <gafurjanibekov91@gmail.com>
2023-04-25 22:27:22 +0500

    h1 added

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
       <div className='flex justify-center items-center w-full min-h-[100vh] bg-indigo-500'>
         <div className='w-[450px] bg-white rounded-md p-4'>
           <div className='"w-full flex justify-center items-center flex-col gap-6'>
-            <h1>Todo List</h1>
+            <h1 className='text-3xl text-gray-500 font-bold'>Todo List</h1>
             <Form />
           </div>
           <Todos />
